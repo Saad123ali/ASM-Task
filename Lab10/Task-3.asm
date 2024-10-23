bac3d3e2eb1f2a32e09793678d0274824fc7deea
Saad Ali Jutt <155216987+Saad123ali@users.noreply.github.com>
2024-10-23 09:46:10 +0500

    Lab10 complete

---
+++ Lab10/Task-3.asm
@@ -0,0 +1,33 @@
+subNumbers macro var1, var2
+  
+    mov dl, var1  
+    
+    sub dl, var2  
+    add dl,30h     
+    
+    mov ah,2      
+    int 21h       
+    
+endm
+
+.model small
+.stack 100h
+
+.data 
+var1 db 5   
+var2 db 2    
+
+.code
+
+main proc 
+    mov ax, @data   
+    mov ds, ax 
+
+    subNumbers var1, var2 
+
+    mov ah, 4ch     
+    int 21h    
+main endp
+
+end main
+                      ;https://github.com/Saad123ali/ASM-Task.git
---
+++ Lab10/Task-4.asm
@@ -0,0 +1,33 @@
+printChar macro ch    
+    mov dl, ch        
+    mov ah, 2         
+    int 21h           
+endm
+
+.model small
+.stack 100h
+
+.data 
+    var1 db '?'       
+
+.code
+
+
+main proc 
+    mov ax, @data    
+    mov ds, ax
+    
+    mov ah, 1         
+    int 21h          
+    mov var1, al   
+
+    printChar var1    
+
+    mov ah, 4ch       
+    int 21h    
+main endp
+
+end main          
+
+
+              ;;  https://github.com/Saad123ali/ASM-Task.git
